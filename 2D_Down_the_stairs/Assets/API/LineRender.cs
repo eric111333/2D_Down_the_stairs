e19d77d36c259f6517d076ae1dcd73163a4e96ad
eric111333 <67904763+eric111333@users.noreply.github.com>
2020-08-12 21:03:43 +0800

    0812

---
+++ 2D_Down_the_stairs/Assets/API/LineRender.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Principal;
+using UnityEngine;
+using System.Collections;
+
+public class LineRender : MonoBehaviour
+{
+    LineRenderer line;
+    public Transform star;
+    public Transform end;
+
+    void Start()
+    {
+        line = GetComponent<LineRenderer>();
+    }
+
+    void Update()
+    {
+        line.SetPosition(0,star.position);
+        line.SetPosition(1,end.position);
+
+    }
+
+}
